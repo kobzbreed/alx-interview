abd5d0afde2f91f7a83d79013a3610721d1fad25
jite <jitekobimowe10@gmail.com>
2023-08-15 14:26:59 +0100

    Create 0-starwars_characters.js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, (err, res, body) => {
+    if (err) {
+        console.log(err);
+    }
+    const characters = JSON.parse(body).characters;
+    const promises = characters.map((character) => {
+        return new Promise((resolve, reject) => {
+            request(character, (err, res, body) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(JSON.parse(body).name);
+            });
+        });
+    }
+    );
+    Promise.all(promises).then((names) => {
+        names.forEach((name) => {
+            console.log(name);
+        });
+    }
+    );
+});
