8367651f2ef47ef455e73922f80ddc5a89014e41
jite <jitekobimowe10@gmail.com>
2023-08-15 13:54:12 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_traingle/0-pascal_triangle.py
+++ 0x00-pascal_traingle/0-pascal_triangle.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-"""Solving Pascal Triangle"""
+"""
+0-pascal_triangle module
+"""
 
 
 def pascal_triangle(n):
-    """Solving of Pascal triangle"""
-    if type(n) is not int:
-        return ("n should be an integer")
+    """
+    returns a list of of integers representing the
+    pascal's triangle of n.
+    """
     if n <= 0:
         return []
-    if n == 1:
-        return [[1]]
-    if n == 2:
-        return [[1], [1, 1]]
 
-    sub = []
-    step = []
-    final = []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
 
-    for i in range(1, n+1):
-        if i == 1:
-            final.append([1])
-        elif i == 2:
-            final.append([1, 1])
-        elif i == 3:
-            final.append([1, 2, 1])
-        else:
-            sub = final[i - 2]
-            for j in range(1, len(sub) + 1):
-                if j == 1:
-                    step.append(1)
-                if j == len(sub):
-                    step.append(1)
-                else:
-                    step.append(sub[j-1] + sub[j])
-
-        if i > 3:
-            final.append(step)
-            step = []
-    return (final)
+    return triangle
